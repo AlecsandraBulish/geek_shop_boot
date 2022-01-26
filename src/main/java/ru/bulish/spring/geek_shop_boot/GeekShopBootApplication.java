ef30007b38521631e95d4867e5893c318911b48e
andress <jump-2005@yandex.ru>
2022-01-26 09:43:48 +0300

    flyway changed ip and password for user

--- src/main/java/ru/bulish/spring/geek_shop_boot/GeekShopBootApplication.java
+++ src/main/java/ru/bulish/spring/geek_shop_boot/GeekShopBootApplication.java
@@ -9,7 +9,7 @@ public class GeekShopBootApplication {
 
     public static void main(String[] args) {
         Flyway flyway = Flyway.configure()
-                .dataSource("jdbc:postgresql://192.168.99.100:5432/shop", "postgres", "bulish")
+                .dataSource("jdbc:postgresql://localhost:5432/shop", "postgres", "password")
                 .load();
         flyway.migrate();
         SpringApplication.run(GeekShopBootApplication.class, args);
